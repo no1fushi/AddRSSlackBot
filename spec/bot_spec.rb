6f5fa31b8cd51b67f0a0c0748c7851b15819df8c
no1fushi <no1fushi@live.jp>
2021-04-02 20:27:52 +0900

    add test

---
+++ spec/bot_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require './lib/bot'
+
+RSpec.describe 'generate_feed' do
+  it 'When the input is a feed' do
+    $stdin = StringIO.new('https://developers-jp.googleblog.com/feeds/posts/default')
+    expect(AddRSSlackBot::Bot.generate_feed).to eq('https://developers-jp.googleblog.com/feeds/posts/default')
+  end
+
+  it 'When the input is a URL' do
+    $stdin = StringIO.new('https://developers-jp.googleblog.com')
+    expect(AddRSSlackBot::Bot.generate_feed).to eq('https://developers-jp.googleblog.com/feeds/posts/default')
+  end
+
+  it 'Input is not recognized when a URL' do
+    $stdin = StringIO.new('aaaa')
+    expect(AddRSSlackBot::Bot.generate_feed).to eq(nil)
+  end
+end
